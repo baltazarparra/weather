4ede917961a838226239c6f4c60ec335114da16c
baltz <baltazarparra@outlook.com>
2022-01-25 09:56:07 -0300

    chore: install nextjs

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Weather</title>
+        <meta name="description" content="Weather App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Weather</h1>
+      </main>
+
+    </div>
+  )
+}
